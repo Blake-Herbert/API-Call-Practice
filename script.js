02df9bec06e84d01e40e50e2d88c9bcf3ed5d9d6
Blake Herbert <Bwherb1@gmail.com>
2023-09-26 20:49:21 -0400

    Refacted code

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
 const button = document.getElementById("button");
 button.addEventListener("click", SendGetRequest);
 
-function SendGetRequest()
+async function SendGetRequest()
 {
-const xhttpr = new XMLHttpRequest();
-//xhttpr.open('GET', "https://catfact.ninja/fact");
-xhttpr.open('GET', "https://localhost:7251/api/HighScores")
-xhttpr.send();
-  
-xhttpr.onload = ()=> 
-{
-  console.log(xhttpr.status);
-  if (xhttpr.status === 200) 
-  {
-      const response = JSON.parse(xhttpr.response);
-      console.log(response);
-  } else 
-    {
-      console.log("Error");
-    }
-  };
+//fetch data
+let data = null;
+try {
+const response = await fetch("https://localhost:7251/api/HighScores", {
+  method: "GET",
+  headers : {
+    'Access-Control-Allow-Origin' : '*'
+  }
+});
+console.log(response);
+data = await response.json();
+console.log(data);
+} catch(error){
+console.log(error);
+}
+
+// check if call worked 
+
+// console log data
+// console log error
+
+
+
+// const xhttpr = new XMLHttpRequest();
+// //xhttpr.open('GET', "https://catfact.ninja/fact");
+// xhttpr.open('GET', "https://localhost:7251/api/HighScores")
+// xhttpr.send();
+// xhttpr.onload = ()=> 
+// {
+//   console.log(xhttpr.status);
+//   if (xhttpr.status === 200) 
+//   {
+//       const response = JSON.parse(xhttpr.response);
+//       console.log(response);
+//   } else 
+//     {
+//       console.log("Error");
+//     }
+//   };
 }
