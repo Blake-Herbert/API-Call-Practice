3f5d1d082434579bae936c1478b025194750162b
Blake <bwherb1@gmail.com>
2025-02-18 18:16:06 -0500

    Fixed curly brace placement

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const button = document.getElementById("callButton");
 button.addEventListener("click", SendGetRequest);
 
-async function SendGetRequest()
-{
+async function SendGetRequest() {
   let data = null;
   try {
     const response = await fetch("https://catfact.ninja/fact", {
@@ -15,6 +14,6 @@ async function SendGetRequest()
     console.log(data);
     window.alert(data.fact);
   } catch(error) {
-  console.log(error);
+    console.log(error);
   }
 }
