76a9de2df888284d594fc1f67e58e99e61fffee6
Blake <bwherb1@gmail.com>
2025-02-18 18:07:12 -0500

    Fixed indentation

--- script.js
+++ script.js
@@ -3,26 +3,18 @@ button.addEventListener("click", SendGetRequest);
 
 async function SendGetRequest()
 {
-//fetch data
-let data = null;
-try {
-const response = await fetch("https://catfact.ninja/fact", {
-  method: "GET",
-  headers : {
-    'Access-Control-Allow-Origin' : '*'
+  let data = null;
+  try {
+  const response = await fetch("https://catfact.ninja/fact", {
+    method: "GET",
+    headers : {
+      'Access-Control-Allow-Origin' : '*'
+    }
+  });
+  data = await response.json();
+  console.log(data);
+  window.alert(data.fact);
+  } catch(error) {
+  console.log(error);
   }
-});
-console.log(response);
-data = await response.json();
-console.log(data);
-window.alert(data.fact);
-} catch(error) {
-console.log(error);
-}
-
-// check if call worked 
-
-// console log data
-// console log error
-
 }
